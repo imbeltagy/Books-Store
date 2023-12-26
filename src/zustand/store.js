58df44db2b474135dfe86262d6926426fe2f6bbb
imbeltagy <justbeltagy@gmail.com>
2023-12-26 20:31:44 +0200

    removed unused files

--- src/zustand/store.js
+++
@@ -1,7 +0,0 @@
-import { create } from "zustand";
-
-export const useSidebarStore = create((set) => ({
-  isSidebarOpen: false,
-  openSidebar: () => set((state) => ({ isSidebarOpen: true })),
-  closeSidebar: () => set((state) => ({ isSidebarOpen: false })),
-}));
